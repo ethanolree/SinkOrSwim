3fe412e398f4a6951c6a2692bed7727819c61514
Ethan Olree <ethanolree@gmail.com>
2022-09-06 21:14:20 -0500

    Initial Commit

---
+++ SinkOrSwim/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SinkOrSwim
+//
+//  Created by Ethan Olree on 9/6/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
