ce0a2bd3a3fd1b2899cb1345477fea865cbd2814
Ethan Olree <ethanolree@gmail.com>
2022-09-06 21:29:35 -0500

    added image

--- SinkOrSwim/ViewController.swift
+++ SinkOrSwim/ViewController.swift
@@ -9,9 +9,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var ImageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        self.ImageView.image = UIImage.init(named: "breakfastClub")
     }
 
 
