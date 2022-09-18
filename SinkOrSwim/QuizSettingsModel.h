d1c3eae3fc510d6ffdd5b267e84f1417a04c3f4b
agregory515 <alexmarkgregory@gmail.com>
2022-09-18 14:01:48 -0500

    Scores

---
+++ SinkOrSwim/QuizSettingsModel.h
@@ -0,0 +1,21 @@
+//
+//  QuizSettingsModel.h
+//  SinkOrSwim
+//
+//  Created by Alex Gregory on 9/17/22.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface QuizSettingsModel : NSObject
+
+-(void)setNoOfQuestions:(NSInteger)value;
+-(void)setHintsYesOrNo:(BOOL)value;
+-(NSInteger)getNoOfQuestions;
+-(BOOL)getHintsYesOrNo;
+
+@end
+
+NS_ASSUME_NONNULL_END
